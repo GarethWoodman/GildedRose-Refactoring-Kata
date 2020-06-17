06574485f72d6986c697f808f07986ac46cb8b52
GarethWoodman <gareth.woodman92@gmail.com>
2020-06-17 11:12:16 +0100

    Tests if quality of item is negative when sell_in is negative

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -33,6 +33,13 @@ describe GildedRose do
           expect(items[0].sell_in).to eq -1
           expect(items[0].quality).to eq 4
         end
+
+        it "quality of item is never negative" do
+          items = [Item.new("foo", 0, 0)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq -1
+          expect(items[0].quality).to eq 0
+        end
       end
     end
   end
