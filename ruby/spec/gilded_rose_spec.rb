5f5f2dcd0ebbff8431584d879308c8a3d80a3f12
GarethWoodman <gareth.woodman92@gmail.com>
2020-06-17 10:59:58 +0100

    tests sell_in and quantity values if sell_in is positive

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -8,6 +8,14 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
-  end
 
+    context 'normal items with positive sell_in value' do
+      it "decreases sell_in and quanity by 1" do
+        items = [Item.new("foo", 10, 20)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 9
+        expect(items[0].quality).to eq 19
+      end
+    end
+  end
 end
