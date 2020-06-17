d3d9178d15b37b9a6229edafeeb2544cf1e64d33
GarethWoodman <gareth.woodman92@gmail.com>
2020-06-17 11:50:33 +0100

    Tests Backstage passes quality if sell_in value is 15

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,7 +1,6 @@
 require 'gilded_rose'
 
 describe GildedRose do
-
   describe '#update_quality' do
     it 'does not change the name' do
       items = [Item.new('foo', 0, 0)]
@@ -11,33 +10,29 @@ describe GildedRose do
 
     context 'normal items' do
       context 'postive sell_in value' do
-        it 'decreases sell_in and quanity by 1' do
+        it 'decreases quality by 1' do
           items = [Item.new('foo', 10, 20)]
           GildedRose.new(items).update_quality()
-          expect(items[0].sell_in).to eq 9
           expect(items[0].quality).to eq 19
         end
 
         it 'quality of item is never negative' do
           items = [Item.new('foo', 1, 0)]
           GildedRose.new(items).update_quality()
-          expect(items[0].sell_in).to eq 0
           expect(items[0].quality).to eq 0
         end
       end
 
       context 'negative sell_in value' do
-        it 'decreases sell_in by 1 and quanity by 2' do
+        it 'decreases quality by 2' do
           items = [Item.new('foo', 0, 6)]
           GildedRose.new(items).update_quality()
-          expect(items[0].sell_in).to eq -1
           expect(items[0].quality).to eq 4
         end
 
         it 'quality of item is never negative' do
           items = [Item.new('foo', 0, 0)]
           GildedRose.new(items).update_quality()
-          expect(items[0].sell_in).to eq -1
           expect(items[0].quality).to eq 0
         end
       end
@@ -45,35 +40,40 @@ describe GildedRose do
 
     context 'Aged Brie' do
       context 'postive sell_in value' do
-        it 'decreases sell_in by 1 and increases quality by 1' do
+        it 'increases quality by 1' do
           items = [Item.new('Aged Brie', 1, 6)]
           GildedRose.new(items).update_quality()
-          expect(items[0].sell_in).to eq 0
           expect(items[0].quality).to eq 7
         end
 
         it 'quality is never more than 50' do
           items = [Item.new('Aged Brie', 1, 49)]
           GildedRose.new(items).update_quality()
-          expect(items[0].sell_in).to eq 0
           expect(items[0].quality).to eq 50
         end
       end
 
       context 'negative sell_in value' do
-        it 'decreases sell_in by 1 and increases quality by 2' do
+        it 'increases quality by 2' do
           items = [Item.new('Aged Brie', 0, 6)]
           GildedRose.new(items).update_quality()
-          expect(items[0].sell_in).to eq -1
           expect(items[0].quality).to eq 8
         end
 
         it 'quality is never more than 50' do
           items = [Item.new('Aged Brie', 0, 49)]
           GildedRose.new(items).update_quality()
-          expect(items[0].sell_in).to eq -1
           expect(items[0].quality).to eq 50
         end
+      end
+    end
+
+    context 'Bakstage Passes' do
+      context 'sell_in value of 15'
+      it 'quality increases by 1' do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 15, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 11
       end
     end
   end
