940fcc4067d3358de3756d0e6798648dcf17b640
GarethWoodman <gareth.woodman92@gmail.com>
2020-06-17 12:34:41 +0100

    Refactored 'normal items'

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -7,49 +7,83 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      check_item(item)
     end
   end
+
+  private
+
+  def check_item(item)
+    return aged_brie(item)        if item.name.include? 'Aged Brie'
+    return legendary(item)        if item.name.include? 'Sulfuras'
+    return conjured(item)         if item.name.include? 'Conjured'
+    return backstage_passes(item) if item.name.include? 'Backstage passes'
+    normal(item)
+  end
+
+  def normal(item)
+    item.sell_in.positive? ? (item.quality -= 1) : (item.quality -= 2)
+    item.quality = 0 unless item.quality.positive?
+    item.sell_in -= 1
+  end
+
+  def aged_brie(item)
+  end
+
+  def backstage_passes(item)
+  end
+
+  def legendary(item)
+  end
+
+  def conjured(item)
+  end
 end
+
+# def update_quality()
+#   @items.each do |item|
+#     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+#       if item.quality > 0
+#         if item.name != "Sulfuras, Hand of Ragnaros"
+#           item.quality = item.quality - 1
+#         end
+#       end
+#     else
+#       if item.quality < 50
+#         item.quality = item.quality + 1
+#         if item.name == "Backstage passes to a TAFKAL80ETC concert"
+#           if item.sell_in < 11
+#             if item.quality < 50
+#               item.quality = item.quality + 1
+#             end
+#           end
+#           if item.sell_in < 6
+#             if item.quality < 50
+#               item.quality = item.quality + 1
+#             end
+#           end
+#         end
+#       end
+#     end
+#     if item.name != "Sulfuras, Hand of Ragnaros"
+#       item.sell_in = item.sell_in - 1
+#     end
+#     if item.sell_in < 0
+#       if item.name != "Aged Brie"
+#         if item.name != "Backstage passes to a TAFKAL80ETC concert"
+#           if item.quality > 0
+#             if item.name != "Sulfuras, Hand of Ragnaros"
+#               item.quality = item.quality - 1
+#             end
+#           end
+#         else
+#           item.quality = item.quality - item.quality
+#         end
+#       else
+#         if item.quality < 50
+#           item.quality = item.quality + 1
+#         end
+#       end
+#     end
+#   end
+# end
